c7496a4b8e899949a49c48152c556f0511fb052b
MP <dailymphuong@gmail.com>
2024-08-21 20:58:38 +0700

    first commit

---
+++ day1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UFT-8">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <title> Mai Phuong </title>
+</head>
+<body>
+<?php
+echo '<p style="text-align: center; color:green; font-size: 25px">Em chào anh Đức ạ</p>';
+echo '<a target="_blank" href ="https://youtu.be/NaE40w3riSQ?si=3_lxBezFr1WY3dYk"><center> Giới thiệu anh Đức bài hát này hihi ^^ </center></a>'
+?>   
+</html>
